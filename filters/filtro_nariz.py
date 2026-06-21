ceeb742e18e68d387211236d0b4771a7e2cefc48
Luis Romero <leromerov28@gmail.com>
2026-06-21 11:49:32 -0600

    1.5

--- filters/filtro_nariz.py
+++ filters/filtro_nariz.py
@@ -2,6 +2,15 @@ import cv2
 from .base_filter import BaseFilter
 
 class FiltroNariz(BaseFilter):
+    def __init__(self, facedetector):
+        self.face_detector = facedetector
+
     def apply(self, frame, landmarks):
-        if landmarks and len(landmarks)>0:
-            pass
+        if not landmarks:
+            return frame
+
+        face_landmark = landmarks[0].landmark
+        nose_landmark = face_landmark[1]
+        x, y = self.face_detector.landmark_to_coordinate(frame, nose_landmark)
+        cv2.circle(frame, (x, y), 8, (0, 0, 250), -1)
+        return frame
